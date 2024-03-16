6fa197466b492039ca156be50bde0ea361515530
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-03-16 02:46:29 -0300

    feat: add function to export notes to csv file

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,6 +2,7 @@ const notesContainer = document.querySelector("#notes-container");
 const noteInput = document.querySelector("#note-content");
 const addNoteBtn = document.querySelector(".add-note");
 const searchInput = document.querySelector("#search-input");
+const exportBtn = document.querySelector("#export-notes");
 
 function showNotes() {
   cleanNotes();
@@ -161,6 +162,22 @@ function searchNotes(search) {
   showNotes();
 }
 
+function exportData() {
+  const notes = getNotes();
+  const csvString = [
+    ["ID", "Conteúdo", "Fixado?"],
+    ...notes.map((note) => [note.id, note.content, note.fixed]),
+  ]
+    .map((e) => e.join(","))
+    .join("\n");
+
+  const element = document.createElement("a");
+  element.href = "data:text/csv;charset=utf-8," + encodeURI(csvString);
+  element.target = "_blank";
+  element.download = "notes.csv";
+  element.click();
+}
+
 addNoteBtn.addEventListener("click", () => addNote());
 
 searchInput.addEventListener("keyup", (e) => {
@@ -170,6 +187,10 @@ searchInput.addEventListener("keyup", (e) => {
 
 noteInput.addEventListener("keydown", (e) => {
   if (e.key === "Enter") addNote();
+});
+
+exportBtn.addEventListener("click", () => {
+  exportData();
 });
 
 showNotes();
